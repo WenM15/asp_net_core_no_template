7195e052f9428352f1d796bdc97b4a3febc2aaf3
WenMing <wenmingleong01@gmail.com>
2025-12-18 00:57:46 +0800

    Implement minimal web app.

---
+++ asp_net_core_no_template/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace asp_net_core_no_template
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            // Simple route
+            app.MapGet("/", () => "Replying to GET /");
+            app.Run();
+        }
+    }
+}
