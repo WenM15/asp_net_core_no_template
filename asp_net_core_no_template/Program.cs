81c5b3b264f5337d98926c68a8dee6aa4bde6d98
WenMing <wenmingleong01@gmail.com>
2025-12-20 17:52:41 +0800

    Add endpoint with route values.

--- asp_net_core_no_template/Program.cs
+++ asp_net_core_no_template/Program.cs
@@ -8,6 +8,8 @@
             var app = builder.Build();
 
             app.MapGet("/", () => "Replying to GET /");
+
+            // Display details of incoming request.
             app.MapGet("/echo", (HttpContext context) =>
             {
                 return Results.Ok(new
@@ -19,7 +21,32 @@
                         q => q.Value.ToString()
                     )
                 });
-            });        
+            });
+
+            // Route values.
+            app.MapGet("status/{service}", (string service) =>
+            {
+                // Assume our web app provides a service called SimulatedDatabase.
+                if (service == "SimulatedDatabase")
+                {
+                    return Results.Ok(new
+                    {
+                        Service = service,
+                        Status = "OK"
+                    }
+                        );
+                }
+                else
+                {
+                    return Results.NotFound(new
+                    {
+                        Service = service,
+                        Error = "Service not found"
+                    }
+
+                        );
+                }
+            });
             app.Run();
         }
     }
