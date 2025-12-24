2f34b0069e0121d6a777ef8d6dc78119407789eb
WenMing <wenmingleong01@gmail.com>
2025-12-24 11:53:03 +0800

    Add conditional middleware to not log echo requests.

--- asp_net_core_no_template/Program.cs
+++ asp_net_core_no_template/Program.cs
@@ -9,26 +9,37 @@
 
             // Add middleware.
             app.Use(async (context, next) =>
-            {
-                var start_time = DateTime.UtcNow;
+            {             
+                // <<<<<<<<-----[ Before endpoint ]----->>>>>>>>
 
-                // Before endpoint.
-                Console.WriteLine(
-                    $"Received request: {context.Request.Method} {context.Request.Path}"
-                );
+                // Conditional middleware. Don't log for echo requests.
+                var path = context.Request.Path;
+                var should_log = !path.StartsWithSegments("/echo");
+
+                if (should_log)
+                {
+                    Console.WriteLine(
+                        $"Received request: {context.Request.Method} {context.Request.Path}"
+                    );
+                }
+
+                var start_time = DateTime.UtcNow;
 
                 // Execute the next middleware or endpoint, then resume here after it finishes.                
                 await next();
 
-                // After endpoint.
+                // <<<<<<<<-----[ After endpoint ]----->>>>>>>>
                 var end_time = DateTime.UtcNow;
                 var elapsed_ms = end_time - start_time;
 
-                Console.WriteLine(
-                    $"Request processed: {context.Request.Method} {context.Request.Path}\n" +
-                    $"Status: {context.Response.StatusCode}\n" + 
-                    $"Elapsed time: {elapsed_ms}ms"
-                );
+                if (should_log)
+                {
+                    Console.WriteLine(
+                        $"Request processed: {context.Request.Method} {context.Request.Path}\n" +
+                        $"Status: {context.Response.StatusCode}\n" +
+                        $"Elapsed time: {elapsed_ms}ms"
+                    );
+                }                
             });
 
             app.MapGet("/", () => "Replying to GET /");
